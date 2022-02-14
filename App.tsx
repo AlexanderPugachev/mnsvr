6b56be815cd360dad042419283ecd25304cb2d1b
alexscare <al.pugachev96@gmail.com>
2022-02-14 19:50:16 +0300

    remove unused dependencies

--- App.tsx
+++ App.tsx
@@ -1,31 +1,25 @@
-import React from 'react';
-import {StatusBar} from 'expo-status-bar';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import { Navigation } from './navigation';
-import {ApplicationProvider, IconRegistry} from "@ui-kitten/components";
-import * as eva from '@eva-design/eva';
-import {EvaIconsPack} from "@ui-kitten/eva-icons";
-import {Provider as PaperProvider} from "react-native-paper";
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import { Navigation } from "./navigation";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
-    const isLoadingComplete = useCachedResources();
-    const colorScheme = useColorScheme();
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
 
-    if (!isLoadingComplete) return null;
-    return (
-        <React.Fragment>
-            <IconRegistry icons={EvaIconsPack}/>
-            <PaperProvider>
-                <ApplicationProvider {...eva} theme={eva[colorScheme]}>
-                    <SafeAreaProvider>
-                        <Navigation colorScheme={colorScheme}/>
-                        <StatusBar/>
-                    </SafeAreaProvider>
-                </ApplicationProvider>
-            </PaperProvider>
-        </React.Fragment>
-    );
+  if (!isLoadingComplete) return null;
+  return (
+    <React.Fragment>
+      <PaperProvider>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </PaperProvider>
+    </React.Fragment>
+  );
 }
